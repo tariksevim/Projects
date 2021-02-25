1e4219242860982907831cd9e5ff71765cccf505
tariksevim <78597758+tariksevim@users.noreply.github.com>
2021-02-25 22:30:39 +0300

    Add files via upload

---
+++ Counter/js.js
@@ -0,0 +1,9 @@
+var x=0;
+function add() {
+    x=x+1;
+    document.getElementById("rakam").innerHTML = x;
+}
+function lower() {
+    x=x-1;
+    document.getElementById("rakam").innerHTML = x;
+}
